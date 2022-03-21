313fc67074d41d21488f033b43f4fa4c9f6137d2
DayaaaanAK <95142739+DayaaaanAK@users.noreply.github.com>
2022-03-22 01:23:14 +0600

    Add files via upload

---
+++ LabTask11/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace LabTask11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cell_1x1 = new System.Windows.Forms.Button();
+            this.Cell_1x2 = new System.Windows.Forms.Button();
+            this.Cell_2x1 = new System.Windows.Forms.Button();
+            this.Cell_2x2 = new System.Windows.Forms.Button();
+            this.Cell_2x3 = new System.Windows.Forms.Button();
+            this.Cell_3x1 = new System.Windows.Forms.Button();
+            this.Cell_3x2 = new System.Windows.Forms.Button();
+            this.Cell_3x3 = new System.Windows.Forms.Button();
+            this.Cell_1x3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Score_1 = new System.Windows.Forms.Label();
+            this.Score_2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Cell_1x1
+            // 
+            this.Cell_1x1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cell_1x1.Location = new System.Drawing.Point(337, 121);
+            this.Cell_1x1.Name = "Cell_1x1";
+            this.Cell_1x1.Size = new System.Drawing.Size(112, 101);
+            this.Cell_1x1.TabIndex = 0;
+            this.Cell_1x1.UseVisualStyleBackColor = true;
+            this.Cell_1x1.Click += new System.EventHandler(this.OnClick_1x1);
+            // 
+            // Cell_1x2
+            // 
+            this.Cell_1x2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cell_1x2.Location = new System.Drawing.Point(455, 121);
+            this.Cell_1x2.Name = "Cell_1x2";
+            this.Cell_1x2.Size = new System.Drawing.Size(112, 101);
+            this.Cell_1x2.TabIndex = 1;
+            this.Cell_1x2.UseVisualStyleBackColor = true;
+            this.Cell_1x2.Click += new System.EventHandler(this.OnClick_1x2);
+            // 
+            // Cell_2x1
+            // 
+            this.Cell_2x1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cell_2x1.Location = new System.Drawing.Point(337, 228);
+            this.Cell_2x1.Name = "Cell_2x1";
+            this.Cell_2x1.Size = new System.Drawing.Size(112, 101);
+            this.Cell_2x1.TabIndex = 3;
+            this.Cell_2x1.UseVisualStyleBackColor = true;
+            this.Cell_2x1.Click += new System.EventHandler(this.OnClick_2x1);
+            // 
+            // Cell_2x2
+            // 
+            this.Cell_2x2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cell_2x2.Location = new System.Drawing.Point(455, 228);
+            this.Cell_2x2.Name = "Cell_2x2";
+            this.Cell_2x2.Size = new System.Drawing.Size(112, 101);
+            this.Cell_2x2.TabIndex = 4;
+            this.Cell_2x2.UseVisualStyleBackColor = true;
+            this.Cell_2x2.Click += new System.EventHandler(this.OnClick_2x2);
+            // 
+            // Cell_2x3
+            // 
+            this.Cell_2x3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cell_2x3.Location = new System.Drawing.Point(573, 228);
+            this.Cell_2x3.Name = "Cell_2x3";
+            this.Cell_2x3.Size = new System.Drawing.Size(112, 101);
+            this.Cell_2x3.TabIndex = 5;
+            this.Cell_2x3.UseVisualStyleBackColor = true;
+            this.Cell_2x3.Click += new System.EventHandler(this.OnClick_2x3);
+            // 
+            // Cell_3x1
+            // 
+            this.Cell_3x1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cell_3x1.Location = new System.Drawing.Point(337, 335);
+            this.Cell_3x1.Name = "Cell_3x1";
+            this.Cell_3x1.Size = new System.Drawing.Size(112, 101);
+            this.Cell_3x1.TabIndex = 6;
+            this.Cell_3x1.UseVisualStyleBackColor = true;
+            this.Cell_3x1.Click += new System.EventHandler(this.OnClick_3x1);
+            // 
+            // Cell_3x2
+            // 
+            this.Cell_3x2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cell_3x2.Location = new System.Drawing.Point(455, 335);
+            this.Cell_3x2.Name = "Cell_3x2";
+            this.Cell_3x2.Size = new System.Drawing.Size(112, 101);
+            this.Cell_3x2.TabIndex = 7;
+            this.Cell_3x2.UseVisualStyleBackColor = true;
+            this.Cell_3x2.Click += new System.EventHandler(this.OnClick_3x2);
+            // 
+            // Cell_3x3
+            // 
+            this.Cell_3x3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cell_3x3.Location = new System.Drawing.Point(573, 335);
+            this.Cell_3x3.Name = "Cell_3x3";
+            this.Cell_3x3.Size = new System.Drawing.Size(112, 101);
+            this.Cell_3x3.TabIndex = 8;
+            this.Cell_3x3.UseVisualStyleBackColor = true;
+            this.Cell_3x3.Click += new System.EventHandler(this.OnClick_3x3);
+            // 
+            // Cell_1x3
+            // 
+            this.Cell_1x3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cell_1x3.Location = new System.Drawing.Point(573, 121);
+            this.Cell_1x3.Name = "Cell_1x3";
+            this.Cell_1x3.Size = new System.Drawing.Size(112, 101);
+            this.Cell_1x3.TabIndex = 9;
+            this.Cell_1x3.UseVisualStyleBackColor = true;
+            this.Cell_1x3.Click += new System.EventHandler(this.OnClick_1x3);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(776, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Player 1 Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(776, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Player 2 Score:";
+            // 
+            // Score_1
+            // 
+            this.Score_1.AutoSize = true;
+            this.Score_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score_1.Location = new System.Drawing.Point(928, 149);
+            this.Score_1.Name = "Score_1";
+            this.Score_1.Size = new System.Drawing.Size(23, 25);
+            this.Score_1.TabIndex = 12;
+            this.Score_1.Text = "0";
+            // 
+            // Score_2
+            // 
+            this.Score_2.AutoSize = true;
+            this.Score_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score_2.Location = new System.Drawing.Point(928, 228);
+            this.Score_2.Name = "Score_2";
+            this.Score_2.Size = new System.Drawing.Size(23, 25);
+            this.Score_2.TabIndex = 13;
+            this.Score_2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 590);
+            this.Controls.Add(this.Score_2);
+            this.Controls.Add(this.Score_1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Cell_1x3);
+            this.Controls.Add(this.Cell_3x3);
+            this.Controls.Add(this.Cell_3x2);
+            this.Controls.Add(this.Cell_3x1);
+            this.Controls.Add(this.Cell_2x3);
+            this.Controls.Add(this.Cell_2x2);
+            this.Controls.Add(this.Cell_2x1);
+            this.Controls.Add(this.Cell_1x2);
+            this.Controls.Add(this.Cell_1x1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Cell_1x1;
+        private System.Windows.Forms.Button Cell_1x2;
+        private System.Windows.Forms.Button Cell_2x1;
+        private System.Windows.Forms.Button Cell_2x2;
+        private System.Windows.Forms.Button Cell_2x3;
+        private System.Windows.Forms.Button Cell_3x1;
+        private System.Windows.Forms.Button Cell_3x2;
+        private System.Windows.Forms.Button Cell_3x3;
+        private System.Windows.Forms.Button Cell_1x3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Score_1;
+        private System.Windows.Forms.Label Score_2;
+    }
+}
+
---
+++ LabTask11/Form1.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabTask11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool player_1 = true;
+        int score_1 = 0;
+        int score_2 = 0;
+        bool WinState()
+        {
+            if (Cell_1x1.Text != "" && Cell_1x1.Text == Cell_1x2.Text && Cell_1x2.Text == Cell_1x3.Text)
+                return true;
+            else if (Cell_2x1.Text != "" && Cell_2x1.Text == Cell_2x2.Text && Cell_2x2.Text == Cell_2x3.Text)
+                return true;
+            else if (Cell_3x1.Text != "" && Cell_3x1.Text == Cell_3x2.Text && Cell_3x2.Text == Cell_3x3.Text)
+                return true;
+            else if (Cell_1x1.Text != "" && Cell_1x1.Text == Cell_2x1.Text && Cell_2x1.Text == Cell_3x1.Text)
+                return true;
+            else if (Cell_1x2.Text != "" && Cell_1x2.Text == Cell_2x2.Text && Cell_2x2.Text == Cell_3x2.Text)
+                return true;
+            else if (Cell_1x3.Text != "" && Cell_1x3.Text == Cell_2x3.Text && Cell_2x3.Text == Cell_3x3.Text)
+                return true;
+            else if (Cell_1x1.Text != "" && Cell_1x1.Text == Cell_2x2.Text && Cell_2x2.Text == Cell_3x3.Text)
+                return true;
+            else if (Cell_3x1.Text != "" && Cell_3x1.Text == Cell_2x2.Text && Cell_2x2.Text == Cell_1x3.Text)
+                return true;
+            else return false;
+        }
+        bool DrawState()
+        {
+            if (Cell_1x1.Text != "" && Cell_1x2.Text != "" && Cell_1x3.Text != "" && Cell_2x1.Text != "" && Cell_2x2.Text != "" && Cell_2x3.Text != "" && Cell_3x1.Text != "" && Cell_3x2.Text != "" && Cell_3x3.Text != "" && WinState() == false)
+                return true;
+            else return false;
+        }
+        void clear()
+        {
+            Cell_1x1.Text = "";
+            Cell_2x1.Text = "";
+            Cell_3x1.Text = "";
+            Cell_1x2.Text = "";
+            Cell_2x2.Text = "";
+            Cell_3x2.Text = "";
+            Cell_1x3.Text = "";
+            Cell_2x3.Text = "";
+            Cell_3x3.Text = "";
+        }
+
+        private void OnClick_1x1(object sender, EventArgs e)
+        {
+            if(Cell_1x1.Text != "")
+            {
+                MessageBox.Show("Already filled");
+            }
+            else
+            {
+                if (player_1 == true)
+                {
+                    Cell_1x1.Text = "X";
+                }
+                else
+                {
+                    Cell_1x1.Text = "O";
+                }
+                player_1 = !player_1;
+            }
+            if(WinState() == true)
+            {
+                if(player_1 == true)
+                {
+                    string result = "Player 1 wins";
+                    MessageBox.Show(result);
+                    score_1++;
+                    Score_1.Text = Convert.ToString(score_1);
+                }
+                else
+                {
+                    string result = "Player 2 wins";
+                    MessageBox.Show(result);
+                    score_2++;
+                    Score_2.Text = Convert.ToString(score_2);
+                }
+                clear();
+            }
+            if(DrawState() == true)
+            {
+                MessageBox.Show("It is a draw");
+                clear();
+            }
+        }
+
+        private void OnClick_1x2(object sender, EventArgs e)
+        {
+            if (Cell_1x2.Text != "")
+            {
+                MessageBox.Show("Already filled");
+            }
+            else
+            {
+                if (player_1 == true)
+                {
+                    Cell_1x2.Text = "X";
+                }
+                else
+                {
+                    Cell_1x2.Text = "O";
+                }
+                
+            }
+            if (WinState() == true)
+            {
+                if (player_1 == true)
+                {
+                    string result = "Player 1 wins";
+                    MessageBox.Show(result);
+                    score_1++;
+                    Score_1.Text = Convert.ToString(score_1);
+                }
+                else
+                {
+                    string result = "Player 2 wins";
+                    MessageBox.Show(result);
+                    score_2++;
+                    Score_2.Text = Convert.ToString(score_2);
+                }
+                clear();
+            }
+            if (DrawState() == true)
+            {
+                MessageBox.Show("It is a draw");
+                clear();
+            }
+            player_1 = !player_1;
+        }
+
+        private void OnClick_1x3(object sender, EventArgs e)
+        {
+            if (Cell_1x3.Text != "")
+            {
+                MessageBox.Show("Already filled");
+            }
+            else
+            {
+                if (player_1 == true)
+                {
+                    Cell_1x3.Text = "X";
+                }
+                else
+                {
+                    Cell_1x3.Text = "O";
+                }
+            }
+            if (WinState() == true)
+            {
+                if (player_1 == true)
+                {
+                    string result = "Player 1 wins";
+                    MessageBox.Show(result);
+                    score_1++;
+                    Score_1.Text = Convert.ToString(score_1);
+                }
+                else
+                {
+                    string result = "Player 2 wins";
+                    MessageBox.Show(result);
+                    score_2++;
+                    Score_2.Text = Convert.ToString(score_2);
+                }
+                clear();
+            }
+            if (DrawState() == true)
+            {
+                MessageBox.Show("It is a draw");
+                clear();
+            }
+            player_1 = !player_1;
+        }
+
+        private void OnClick_2x3(object sender, EventArgs e)
+        {
+            if (Cell_2x3.Text != "")
+            {
+                MessageBox.Show("Already filled");
+            }
+            else
+            {
+                if (player_1 == true)
+                {
+                    Cell_2x3.Text = "X";
+                }
+                else
+                {
+                    Cell_2x3.Text = "O";
+                }
+            }
+            if (WinState() == true)
+            {
+                if (player_1 == true)
+                {
+                    string result = "Player 1 wins";
+                    MessageBox.Show(result);
+                    score_1++;
+                    Score_1.Text = Convert.ToString(score_1);
+                }
+                else
+                {
+                    string result = "Player 2 wins";
+                    MessageBox.Show(result);
+                    score_2++;
+                    Score_2.Text = Convert.ToString(score_2);
+                }
+                clear();
+            }
+            if (DrawState() == true)
+            {
+                MessageBox.Show("It is a draw");
+                clear();
+            }
+            player_1 = !player_1;
+        }
+
+        private void OnClick_3x3(object sender, EventArgs e)
+        {
+            if (Cell_3x3.Text != "")
+            {
+                MessageBox.Show("Already filled");
+            }
+            else
+            {
+                if (player_1 == true)
+                {
+                    Cell_3x3.Text = "X";
+                }
+                else
+                {
+                    Cell_3x3.Text = "O";
+                }
+            }
+            if (WinState() == true)
+            {
+                if (player_1 == true)
+                {
+                    string result = "Player 1 wins";
+                    MessageBox.Show(result);
+                    score_1++;
+                    Score_1.Text = Convert.ToString(score_1);
+                }
+                else
+                {
+                    string result = "Player 2 wins";
+                    MessageBox.Show(result);
+                    score_2++;
+                    Score_2.Text = Convert.ToString(score_2);
+                }
+                clear();
+            }
+            if (DrawState() == true)
+            {
+                MessageBox.Show("It is a draw");
+                clear();
+            }
+            player_1 = !player_1;
+        }
+
+        private void OnClick_3x2(object sender, EventArgs e)
+        {
+            if (Cell_3x2.Text != "")
+            {
+                MessageBox.Show("Already filled");
+            }
+            else
+            {
+                if (player_1 == true)
+                {
+                    Cell_3x2.Text = "X";
+                }
+                else
+                {
+                    Cell_3x2.Text = "O";
+                }
+            }
+            if (WinState() == true)
+            {
+                if (player_1 == true)
+                {
+                    string result = "Player 1 wins";
+                    MessageBox.Show(result);
+                    score_1++;
+                    Score_1.Text = Convert.ToString(score_1);
+                }
+                else
+                {
+                    string result = "Player 2 wins";
+                    MessageBox.Show(result);
+                    score_2++;
+                    Score_2.Text = Convert.ToString(score_2);
+                }
+                clear();
+            }
+            if (DrawState() == true)
+            {
+                MessageBox.Show("It is a draw");
+                clear();
+            }
+            player_1 = !player_1;
+        }
+
+        private void OnClick_3x1(object sender, EventArgs e)
+        {
+            if (Cell_3x1.Text != "")
+            {
+                MessageBox.Show("Already filled");
+            }
+            else
+            {
+                if (player_1 == true)
+                {
+                    Cell_3x1.Text = "X";
+                }
+                else
+                {
+                    Cell_3x1.Text = "O";
+                }
+            }
+            if (WinState() == true)
+            {
+                if (player_1 == true)
+                {
+                    string result = "Player 1 wins";
+                    MessageBox.Show(result);
+                    score_1++;
+                    Score_1.Text = Convert.ToString(score_1);
+                }
+                else
+                {
+                    string result = "Player 2 wins";
+                    MessageBox.Show(result);
+                    score_2++;
+                    Score_2.Text = Convert.ToString(score_2);
+                }
+                clear();
+            }
+            if (DrawState() == true)
+            {
+                MessageBox.Show("It is a draw");
+                clear();
+            }
+            player_1 = !player_1;
+        }
+
+        private void OnClick_2x1(object sender, EventArgs e)
+        {
+            if (Cell_2x1.Text != "")
+            {
+                MessageBox.Show("Already filled");
+            }
+            else
+            {
+                if (player_1 == true)
+                {
+                    Cell_2x1.Text = "X";
+                }
+                else
+                {
+                    Cell_2x1.Text = "O";
+                }
+            }
+            if (WinState() == true)
+            {
+                if (player_1 == true)
+                {
+                    string result = "Player 1 wins";
+                    MessageBox.Show(result);
+                    score_1++;
+                    Score_1.Text = Convert.ToString(score_1);
+                }
+                else
+                {
+                    string result = "Player 2 wins";
+                    MessageBox.Show(result);
+                    score_2++;
+                    Score_2.Text = Convert.ToString(score_2);
+                }
+                clear();
+            }
+            if (DrawState() == true)
+            {
+                MessageBox.Show("It is a draw");
+                clear();
+            }
+            player_1 = !player_1;
+        }
+
+        private void OnClick_2x2(object sender, EventArgs e)
+        {
+            if (Cell_2x2.Text != "")
+            {
+                MessageBox.Show("Already filled");
+            }
+            else
+            {
+                if (player_1 == true)
+                {
+                    Cell_2x2.Text = "X";
+                }
+                else
+                {
+                    Cell_2x2.Text = "O";
+                }
+            }
+            if (WinState() == true)
+            {
+                if (player_1 == true)
+                {
+                    string result = "Player 1 wins";
+                    MessageBox.Show(result);
+                    score_1++;
+                    Score_1.Text = Convert.ToString(score_1);
+                }
+                else
+                {
+                    string result = "Player 2 wins";
+                    MessageBox.Show(result);
+                    score_2++;
+                    Score_2.Text = Convert.ToString(score_2);
+                }
+                clear();
+            }
+            if (DrawState() == true)
+            {
+                MessageBox.Show("It is a draw");
+                clear();
+            }
+            player_1 = !player_1;
+        }
+    }
+}
